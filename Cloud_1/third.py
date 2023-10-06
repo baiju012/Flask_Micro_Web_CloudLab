3753750338ae4da5173b99c2c78da454ff2d329c
Baiju kumar <111991510+baiju012@users.noreply.github.com>
2023-10-06 20:19:03 +0530

    Update third.py

--- Cloud_1/third.py
+++ Cloud_1/third.py
@@ -1,1 +1,16 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+
+def welcome():
+    return "Welcome to IBM Faculty Development Program"
+
+@app.route('/Baiju')
+def greet():
+    return "Baiju kumar Here Welcome to all of you"
+
+if __name__=='__main__':
+    app.run(debug=True)
 
