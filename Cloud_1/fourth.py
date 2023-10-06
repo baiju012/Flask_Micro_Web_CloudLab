70829a93220cdff345f157c8aa529cd2fcda1611
Baiju kumar <111991510+baiju012@users.noreply.github.com>
2023-10-06 20:33:07 +0530

    Update fourth.py

--- Cloud_1/fourth.py
+++ Cloud_1/fourth.py
@@ -1,12 +1,25 @@
 ### Building Url Dynamically 
 ####Variable Rules And URL Building
 
+
+
+### Here I am importing Flask and for building of dynamic url you have to use redirect and url_for
+
 from flask import Flask,redirect,url_for
+
+### I Create instance of Flask my instance name is app YOU may choose other
 app=Flask(__name__)
+
+
+### This route is for HOme Page
 
 @app.route('/')
 def welcome():
-    return 'Welcome to my Home Page'
+    return "Welcome to my Home Page"
+
+
+### You may use variable parameter in route    
+###You may provide html whole file or small code
 
 @app.route('/success/<int:score>')
 def success(score):
@@ -17,7 +30,7 @@ def success(score):
 def fail(score):
     return "The Person has failed and the marks is "+ str(score)
 
-### Result checker
+### Result checker using redirect url
 @app.route('/results/<int:marks>')
 def results(marks):
     result=""
@@ -27,6 +40,6 @@ def results(marks):
         result='success'
     return redirect(url_for(result,score=marks))
 
-
+###Here I am assign main to my instance in conndition so that it will run my instance 
 if __name__=='__main__':
     app.run(debug=True)
