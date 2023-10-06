aebe6e7c38fd881d8d9158fc09e0ccf5f3774e92
Baiju kumar <111991510+baiju012@users.noreply.github.com>
2023-10-06 23:00:43 +0530

    Update third.py

--- Cloud_1/third.py
+++ Cloud_1/third.py
@@ -5,17 +5,20 @@
 from flask import Flask
 
 ### I Create instance of Flask my instance name is app YOU may choose other
+###Create an Instance of Flask
 app=Flask(__name__)
 
 
 ### HOme page dirctory 
-@app.route('/')
+### /: This route corresponds to the home page of the web application. When you access the root URL (e.g., http://localhost:5000/),
 
+@app.route('/')
 def welcome():
     return "Welcome to IBM Faculty Development Program"
 
 
 ### This is my second directory when 
+### This route corresponds to a page accessible at http://localhost:5000/Baiju. When you access this URL
 
 @app.route('/Baiju')
 def greet():
@@ -23,6 +26,8 @@ def greet():
 
 
 ### Here I am assign main to my instance in conndition so that it will run my instance 
+### starts the Flask development server with debugging enabled
+
 if __name__=='__main__':
     app.run(debug=True)
 
