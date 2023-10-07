3989bea8b1abf1e8c2c893a267cb3efb5c564e7b
Baiju kumar <111991510+baiju012@users.noreply.github.com>
2023-10-07 19:42:03 +0530

    Update post_6.py

--- Cloud_1/post_6.py
+++ Cloud_1/post_6.py
@@ -1,1 +1,26 @@
+#importing template by module of render_template and request to server
+from flask import Flask,request, render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+
+def welcome():
+  return render_template('post.html')
+
+
+
+@app.route('/login',methods=['POST'])
+
+def login():
+     # Retrieve "uname" and "password" parameters from the form data.
+    uname=request.form.get('uname')
+    password=request.form.get('pass')
+    if uname=='Baiju' and password=='Fet':
+        return 'Welcome %s' %uname
+    else:
+        return "Wrong Credentials"
+    
+if __name__=='__main__':
+    app.run(debug=True)
 
