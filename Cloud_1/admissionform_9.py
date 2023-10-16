498734505d6275f256d0449e161a1c4b66299e90
Baiju kumar <111991510+baiju012@users.noreply.github.com>
2023-10-16 14:14:24 +0530

    Update admissionform_9.py

--- Cloud_1/admissionform_9.py
+++ Cloud_1/admissionform_9.py
@@ -1,30 +1,34 @@
 from flask import Flask, render_template, request
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
 
+@app.route('/', methods=['GET', 'POST'])
 def admission_form():
     if request.method == 'POST':
         name = request.form['name']
         DOB = request.form['DOB']
         phone_number = request.form['phone_number']
         state = request.form['state']
-        mark_12th = request.form['mark_12th']
+        mark_12th = int(request.form['mark_12th'])
         submitted = True
-    
-        #Pass the form data to the template
-        return render_template('admissionform_9.html',
+
+        eligible_for_admission = mark_12th > 400
+
+        # Pass the form data to the template
+        return render_template('admissionform.html',
                                name=name,
                                DOB=DOB,
                                phone_number=phone_number,
                                state=state,
                                mark_12th=mark_12th,
-                               submitted=submitted
+                               submitted=submitted,
+                               eligible_for_admission=eligible_for_admission
+                              
                                )
-    
-    
-    return render_template('admissionform_9.html')
 
-if __name__=='__main__':
+    return render_template('admissionform.html')
+
+
+if __name__ == '__main__':
     app.run(debug=True)
